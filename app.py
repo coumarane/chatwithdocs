6fc2024574f574d5842396838b8b2a4b4ed0b9ba
Coumarane <c.coumarane@gmail.com>
2025-01-09 00:15:08 +0100

    Add index page

--- app.py
+++ app.py
@@ -1,15 +1,61 @@
+import os
+import sys
 from typing import Union
+from fastapi import FastAPI, Request, BackgroundTasks, HTTPException, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.templating import Jinja2Templates
+from contextlib import asynccontextmanager
 
-from fastapi import FastAPI
 
-app = FastAPI()
+DEBUG = os.environ.get("DEBUG", "").strip().lower() in {
+    "1", "true", "on", "yes"}
+
+tags_metadata = [
+    {
+        "name": "Build",
+        "description": "Use this API to check project build number."
+    },
+    {
+        "name": "Chat With Docs",
+        "description": "ChatWithDocs Service APIs"
+    },
+]
+
+resource = {}
 
 
-@app.get("/")
-def read_root():
+@asynccontextmanager
+async def app_lifespan(app: FastAPI):
+    print("init lifespan")
+    resource["msg"] = "Hello World!"
+    yield
+    resource.clear()
+    print("clean up lifespan")
+
+app = FastAPI(
+    debug=DEBUG,
+    title="ChatWithDocs Web Service",
+    description="This project is a production ready ChatWithDocs Web Service"
+    "<br /><br />"
+    "Author - [***Coumarane COUPPANE***](https://www.linkedin.com/https://www.linkedin.com/in/coumarane-couppane-712a2415/)",
+    version="1.0.0",
+    openapi_tags=tags_metadata,
+    docs_url="/swagger/",
+    lifespan=app_lifespan,
+)
+
+templates = Jinja2Templates(directory="src/templates")
+
+
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/test")
+def get_test():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+print("completed app init.")
